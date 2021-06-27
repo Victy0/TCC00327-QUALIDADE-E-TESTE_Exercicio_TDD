7e955e220311a0a04e524b95b06df1fc1778448a
Victor Marques <victyrmarques@gmail.com>
2021-06-26 21:37:55 -0300

    TDD 3 - REFACTOR: processar e gerar pagamento com validações de valores

--- src/pagamento/Pagamento.java
+++ src/pagamento/Pagamento.java
@@ -3,13 +3,13 @@ package pagamento;
 public class Pagamento {
 	
 	private Double valorPago;
+	
+	public Pagamento(Double valorPago) {
+		this.valorPago = valorPago;
+	}
 
 	public Double getValorPago() {
 		return valorPago;
-	}
-
-	public void setValorPago(Double valorPago) {
-		this.valorPago = valorPago;
 	}
 
 }
--- src/processador/Processador.java
+++ src/processador/Processador.java
@@ -7,13 +7,12 @@ import pagamento.Pagamento;
 public class Processador {
 	
 	public Pagamento processa(Boleto boleto, Fatura fatura) {
+		
 		if(boleto != null && fatura != null) 
 		{
 			if(boleto.getValor().equals(fatura.getValorTotal())) 
 			{
-				Pagamento pagamento = new Pagamento();
-				pagamento.setValorPago(fatura.getValorTotal());
-				return pagamento;
+				return new Pagamento(fatura.getValorTotal());
 			}
 		}
 		
