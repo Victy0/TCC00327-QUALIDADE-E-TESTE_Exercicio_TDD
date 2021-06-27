87e13d18c2fa3c0aaaef5cfb98eb641d6c34e40e
Victor Marques <victyrmarques@gmail.com>
2021-06-26 21:14:48 -0300

    TDD 2 - TEST RED: processar e gerar pagamento

--- test/processador/ProcessadorTest.java
+++ test/processador/ProcessadorTest.java
@@ -1,8 +1,11 @@
 package processador;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import boleto.Boleto;
+import fatura.Fatura;
 
 public class ProcessadorTest {
 	
@@ -13,5 +16,23 @@ public class ProcessadorTest {
 		processador = new Processador();
 		Assertions.assertNotNull(processador);
 	}
+	
+	@DisplayName("Testa se recebeu boleto e fatura e gerou pagamento")
+	@Test
+	public void testGeraPagamento() {
+		processador = new Processador();
+		
+		Fatura fatura = new Fatura();
+		fatura.setValorTotal(50.0);
+		fatura.setNomeCliente("Cliente");
+		
+		Boleto boleto = new Boleto();
+		boleto.setValor(50.0);
+		
+		Pagamento pagamento = processador.processa(boleto, fatura);
+		Assertions.assertNotNull(pagamento);
+		 
+	}
+	
 
 }
